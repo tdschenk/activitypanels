3a9e711506c6e243ead0ee78933859f4b5cdc99a
Tyler Schenk <schenk.tyler@gmail.com>
2015-06-23 11:09:52 -0500

    Revert "save pt list"
    This reverts commit a0e4bfaec786247f47b48d9af9de6a9b83164290.

--- R/activitypanels-package.r
+++ R/activitypanels-package.r
@@ -22,7 +22,6 @@ activity.counts <- function(data, params, ...) {
 
 ## WVU Stacked bar chart of activity classifications
 activity_class_bar <- function(data, params, ...) {
-  pts <- unique(data$activity$metadata$pt)
   activity <- data$activity$data
   activity$day <- substr(data$activity$metadata$local_time, 0, 10)
   days <- unique(activity$day)
@@ -42,7 +41,7 @@ activity_class_bar <- function(data, params, ...) {
     add_axis("x", title = "",
              properties = axis_props(labels = list(angle = 45, align = "left"))) %>%
     add_axis("y", title = "Polls") %>%
-    add_axis("x", orient = "top", ticks = 0, title = paste0("Participant: ", paste(pts),collapse=",")),
+    add_axis("x", orient = "top", ticks = 0, title = paste0("Participant: ", paste(unique(data$activity$metadata$pt),collapse=",")),
              properties = axis_props(
                axis = list(stroke = "white"),
                labels = list(fontSize = 0)))
